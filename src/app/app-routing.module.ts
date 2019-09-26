c5d58be9a7460b9f7dd6ccd98a76061b31e2c8f2
chatchai-tangg <chatchai.t@rmutp.ac.th>
2019-09-26 18:27:29 +0700

    no message

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,10 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'menu', loadChildren: './pages/menu/menu.module#MenuPageModule' },
+  { path: 'main', loadChildren: './pages/main/main.module#MainPageModule' },
+  { path: 'staff', loadChildren: './pages/staff/staff.module#StaffPageModule' },
+  { path: 'student', loadChildren: './pages/student/student.module#StudentPageModule' },
 ];
 
 @NgModule({
