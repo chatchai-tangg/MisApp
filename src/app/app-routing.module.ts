9df376845f2f71bd0cb1ea23f47ef41adc07c62c
chatchai-tangg <chatchai.t@rmutp.ac.th>
2019-09-26 19:14:33 +0700

    no message

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,8 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
-  { path: 'menu', loadChildren: './pages/menu/menu.module#MenuPageModule' },
+  { path: '', redirectTo: 'menu', pathMatch: 'full' },
+  { path: 'menu', loadChildren: () => import('./pages/menu/menu.module').then(m => m.MenuPageModule) },
   { path: 'main', loadChildren: './pages/main/main.module#MainPageModule' },
   { path: 'staff', loadChildren: './pages/staff/staff.module#StaffPageModule' },
   { path: 'student', loadChildren: './pages/student/student.module#StudentPageModule' },
--- src/app/pages/menu/menu.page.ts
+++ src/app/pages/menu/menu.page.ts
@@ -7,6 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuPage implements OnInit {
 
+  page = [
+    {
+      title: 'Main',
+      url: '/menu/main',
+      icon: 'home'
+    },
+    {
+      title: 'Subclass',
+      open: false,
+      children: [
+        {
+          title: 'บุคลากร',
+          url: '/menu/staff',
+          icon: 'logo-ionic'
+        },
+        {
+          title: 'นักศึกษา',
+          url: '/menu/student',
+          icon: 'logo-google'
+        }
+      ]
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
